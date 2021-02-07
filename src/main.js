fe775ee8f1a1ff3ca7e46b01ccf5d84f0c4d028f
ChrisBirkenmaier <c.birkenmaier@rrooaarr.com>
2021-02-07 17:53:56 +0100

    Auto updater installiert

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const isDev = require("electron-is-dev");
+require("update-electron-app")();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
+    // eslint-disable-line global-require
+    app.quit();
 }
 
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+    // and load the index.html of the app.
+    mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  if (isDev) {
-    console.log("Running in development");
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-  }
+    if (isDev) {
+        console.log("Running in development");
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools();
+    }
 };
 
 // This method will be called when Electron has finished
@@ -38,15 +39,15 @@ app.on("ready", createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
